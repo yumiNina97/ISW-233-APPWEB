d9e6b96525817f3751491e9e240fc8285e75b2c2
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2025-05-19 14:31:15 -0400

    primer 1dom

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ISW-233-APPWEB
-TecnologiaWebI
+# Aplicaciones Web I - Ejercicio 1
+
+1. Disene el header segun las imagenes provisionadas
+2. Disene el footer segun las imagenes provisionadas
+3. Mediante DOM API y la variable HTML genere los componentes cards
+4. Piense en que otra forma se podria hacer?
+5. Haga responsive el diseno
+
+Considere el box model y los conceptos avanzados en las lecciones
---
+++ index.js
@@ -0,0 +1,23 @@
+// TODO: Use the DOM API to create the card components
+const html = `<article class="card">
+<h3 class="card__title"></h3>
+<div class="card__body">
+  <div class='card__body__image'></div>
+  <section class='card__body__content'>
+  </section>
+</div>
+</article>`;
+
+// 1. Seleccione el container
+
+// 2. cree una funciotn createCardComponent.
+/**
+ * @param {string} title
+ * @param {string} body
+ *
+ * @return {HTMLElement}
+ */
+
+// 3. Cree un componente
+
+// 4. Agrege este nuevo componente al container
